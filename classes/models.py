024636457b9d4fb369d1f3f2330fb3c7dd301e58
Bundit <bundittsp@gmail.com>
2020-02-05 11:41:32 +0700

    initial files for WEEK 4 exercises

--- classes/models.py
+++ classes/models.py
@@ -1,3 +1,64 @@
 from django.db import models
 
 # Create your models here.
+courses = [
+    {
+        'id': 1,
+        'code' : '06016322',
+        'name' : 'WEB PROGRAMMING',
+        'description': 'The course teaches how to build a web application with database using Django framework.'
+    },
+    {
+        'id': 2,
+        'code' : '06026118',
+        'name' : 'IT PROJECT MANAGEMENT',
+        'description': 'The course teaches how to manage IT projects to meet deadline and budget.'
+    }
+]
+classes = [
+    {
+        'id' : 1,
+        'course_id': 1,
+        'section' : 1,
+        'weekday': 'Thursday',
+        'time' : '9:00 - 13:00',
+        'room' : 'Lab304',
+        'student_amount': 50,
+    },
+    {
+        'id' : 2,
+        'course_id': 1,
+        'section' : 2,
+        'weekday': 'Thursday',
+        'time' : '13:30 - 13:00',
+        'room' : 'Lab304',
+        'student_amount': 55,
+    },
+    {
+        'id' : 3,
+        'course_id': 2,
+        'section' : 1,
+        'weekday': 'Tuesday',
+        'time' : '9:00 - 12:00',
+        'room' : '333',
+        'student_amount': 30,
+    }
+]
+
+attendance = [
+    {
+        'class_id': 1,
+        'attend': 49,
+        'absent': 1
+    },
+    {
+        'class_id': 2,
+        'attend': 53,
+        'absent': 2
+    },
+    {
+        'class_id': 3,
+        'attend': 28,
+        'absent': 2
+    },
+]
--- classes/views.py
+++ classes/views.py
@@ -1,13 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from . import models
 
 # Create your views here.
 def index(request):
+    """
+        Index page - หน้าจอรายการวิชาที่มีการสอนทั้งหมด
+    """
     return HttpResponse('Index')
 
 def detail(request, class_id):
+    """
+        Course detail page – เมื่อกด link จากหน้า Index page มาจะได้หน้าจอแสดงรายละเอียดของแต่ละวิชา 
+        (วิชานี้สอนอะไร, มีจำนวนนักเรียนกี่คน, มีคนมาเรียน และขาดกี่คน)
+    """
     return HttpResponse('Class Detail: %s' % class_id)
 
 def check_in(request, class_id):
+    """
+        Check-in page - เมื่อกด link จากหน้า Index page หรือ Course detail page ก็จะพบหน้าจอที่มีชื่อคอร์สอยู่เป็นหัวข้อ 
+        และมี QR code (เป็นรูป static หามาจาก Internet ไม่ต้อง auto-generate)
+    """
     return HttpResponse('Attendance Check-in: %s' % class_id)
