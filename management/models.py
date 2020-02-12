6f1aa3fc59936db35654d81427d99f77a867cb94
Bundit <bundittsp@gmail.com>
2020-02-12 22:23:42 +0700

    Create model ClassRoom in management/models.py

--- management/models.py
+++ management/models.py
@@ -7,3 +7,22 @@ class Course(models.Model):
     description = models.TextField(null=True, blank=True)
     semester = models.CharField(max_length=1)
     academic_year = models.CharField(max_length=4)
+
+
+class ClassRoom(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.PROTECT)
+    section = models.SmallIntegerField()
+    WEEKDAYS = (
+        ('M', 'Monday'),
+        ('T', 'Tuesday'),
+        ('W', 'Wednesday'),
+        ('TH', 'Thursday'),
+        ('F', 'Friday'),
+        ('S', 'Saturday'),
+        ('Su', 'Sunday'),
+    )
+    weekday = models.CharField(max_length=2, choices=WEEKDAYS)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    room = models.CharField(max_length=10)
+    student_amount = models.IntegerField(default=0)
