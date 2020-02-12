39923a21a911d18269d9ef08c88210e8d19f8ce3
Bundit <bundittsp@gmail.com>
2020-02-12 21:58:28 +0700

    create model Course in management/models.py

--- management/models.py
+++ management/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
+    semester = models.CharField(max_length=1)
+    academic_year = models.CharField(max_length=4)
