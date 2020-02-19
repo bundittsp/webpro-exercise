f6ef52efbcef4f1530c08c18f0c3656a07badb93
Bundit <bundittsp@gmail.com>
2020-02-20 00:22:45 +0700

    Exercise 6-3, 6-4: index and classroom detail pages

--- classes/views.py
+++ classes/views.py
@@ -1,16 +1,34 @@
+from astroid import objects
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
-from management.models import Student
+from django.shortcuts import redirect, render
+
+from management.models import ClassRoom, Student
+from classes.models import StudentAttendance
+
 
 # Create your views here.
 def index(request):
     """
         Index page - หน้าจอรายการวิชาที่มีการสอนทั้งหมด
     """
-    search_txt = request.GET.get('inputSearch')
+    search_txt = request.GET.get('inputSearch', '')
     print(search_txt)
+    semester = request.GET.get('semester', '')
+    year = request.GET.get('year', '')
+
+    classes = ClassRoom.objects.filter(
+        course__name__icontains=search_txt
+    )
+    if semester:
+        classes = classes.filter(course__semester=semester)
+    if year:
+        classes = classes.filter(course__academic_year=year)
+
     return render(request, 'classes/index.html', context={
-        'search_txt': search_txt
+        'search_txt': search_txt,
+        'semester': semester,
+        'year': year,
+        'classes': classes
     })
 
 def detail(request, class_id):
@@ -18,7 +36,14 @@ def detail(request, class_id):
         Class detail page – เมื่อกด link จากหน้า Index page มาจะได้หน้าจอแสดงรายละเอียดของแต่ละวิชา 
         (วิชานี้สอนอะไร, มีจำนวนนักเรียนกี่คน, มีคนมาเรียน และขาดกี่คน)
     """
-    return render(request, 'classes/detail.html', context={})
+    classroom = ClassRoom.objects.get(pk=class_id)
+    attend_amount = classroom.students.count()
+    absent_amount = classroom.student_amount - attend_amount
+    return render(request, 'classes/detail.html', context={
+        'classroom': classroom,
+        'attend': attend_amount,
+        'absent': absent_amount
+    })
 
 def check_in(request, class_id):
     """
@@ -27,5 +52,10 @@ def check_in(request, class_id):
         โดยถ้ากดปุ่ม Check in ซ้ำจะเป็นการไปบันทึกเวลาใหม่ในรายการเดิม
     """
     student = Student.objects.get(pk=1)
+    attend = StudentAttendance.objects.create(
+        classroom_id=class_id,
+        student_id=student.id,
+        attend=True
+    )
 
     return redirect(to='class_detail', class_id=class_id)
